f42374a4cd8d4d8d9695824c60764182e87ef3d0
shihao <shisoftgenius@gmail.com>
2017-09-06 07:06:26 +0800

    compose fn id

--- tests/closure_serialize.rs
+++ tests/closure_serialize.rs
@@ -3,18 +3,18 @@ use bifrost_hasher::hash_str;
 
 macro_rules! fn_id {
     ($expr: tt) => {
-        hash_str(stringify!($expr))
+        hash_str(concat!(module_path!(), "::", stringify!($expr)))
     };
 }
 
-macro_rules! seirialize {
+macro_rules! serialize {
     ($fn: expr) => {
-       let func_id = fn_id!($fn) as u64;
+       let func_id = fn_id!($fn);
 
     };
 }
 
 #[test]
 pub fn native_closure() {
-    seirialize!(|x: u32| 1);
+    serialize!(|x: u32| 1);
 }
