05498c0b1c9b048c581bb828950185208e9fd35f
Hao Shi <shisoftgenius@gmail.com>
2017-11-28 10:24:42 +0800

    resource manager and it's triggers

--- src/server/resources/manager.rs
+++ src/server/resources/manager.rs
@@ -1,12 +1,19 @@
 use bifrost::conshash::ConsistentHashing;
+use bifrost::raft::RaftService;
 use bifrost::raft::state_machine::StateMachineCtl;
 use bifrost::raft::state_machine::callback::server::{
     SMCallback, NotifyError};
+use bifrost::membership::raft::commands::{
+    on_group_member_offline, on_group_member_online,
+    on_group_member_joined, on_group_member_left};
+use bifrost::membership::client::{Member as ClientMember};
 use bifrost::utils::bincode;
 use neb::dovahkiin::types::Map;
 use std::collections::BTreeMap;
 use std::sync::Arc;
-use parking_lot::Mutex;
+use parking_lot::RwLock;
+
+pub static DEFAULT_SERVICE_ID: u64 = hash_ident!(HIVEMIND_RESOURCE_MANAGER) as u64;
 
 raft_state_machine! {
     def cmd register_node(node: ComputeNode) | String;
@@ -76,7 +83,7 @@ pub enum OccupationStatus {
 
 
 pub struct ResourceManager {
-    compute_nodes: BTreeMap<u64, ComputeNode>,
+    compute_nodes: Arc<RwLock<BTreeMap<u64, ComputeNode>>>,
     tasks: BTreeMap<u64, Task>,
     callback: SMCallback,
     sm_id: u64
@@ -119,9 +126,62 @@ impl StateMachineCtl for ResourceManager {
         return self.id();
     }
     fn snapshot(&self) -> Option<Vec<u8>> {
-        // data from resource manager will be
-        // generated in runtime only
-        None
+        let nodes = self.compute_nodes.read();
+        Some(bincode::serialize(&(&nodes.clone(), &self.tasks)))
     }
-    fn recover(&mut self, data: Vec<u8>) {}
+    fn recover(&mut self, data: Vec<u8>) {
+        let pack:(BTreeMap<u64, ComputeNode>, BTreeMap<u64, Task>) = bincode::deserialize(&data);
+        self.compute_nodes = Arc::new(RwLock::new(pack.0));
+        self.tasks = pack.1;
+    }
+}
+
+impl ResourceManager {
+    pub fn new(raft: Arc<RaftService>, group_id: u64, membership_cb: &SMCallback) -> ResourceManager {
+        let nodes = Arc::new(RwLock::new(BTreeMap::<u64, ComputeNode>::new()));
+        let manager = ResourceManager{
+            compute_nodes: nodes.clone(),
+            tasks: BTreeMap::new(),
+            callback: SMCallback::new(DEFAULT_SERVICE_ID, raft),
+            sm_id: DEFAULT_SERVICE_ID
+        };
+        let nr1 = nodes.clone();
+        membership_cb.internal_subscribe(
+            &on_group_member_offline::new(&group_id),
+            move |changes| {
+                mark_member(false, changes, &nr1)
+        });
+        let nr2 = nodes.clone();
+        membership_cb.internal_subscribe(
+            &on_group_member_online::new(&group_id),
+            move |changes: &Result<(ClientMember, u64), ()>| {
+                mark_member(true, changes, &nr2)
+            });
+        let nr3 = nodes.clone();
+        membership_cb.internal_subscribe(
+            &on_group_member_joined::new(&group_id),
+            move |changes: &Result<(ClientMember, u64), ()>| {
+                mark_member(true, changes, &nr3)
+            });
+        let nr4 = nodes.clone();
+        membership_cb.internal_subscribe(
+            &on_group_member_left::new(&group_id),
+            move |changes: &Result<(ClientMember, u64), ()>| {
+                mark_member(false, changes, &nr4)
+            });
+        return manager;
+    }
+}
+
+fn mark_member(
+    online: bool,
+    changes: &Result<(ClientMember, u64), ()>,
+    nodes: &Arc<RwLock<BTreeMap<u64, ComputeNode>>>
+) {
+    if let &Ok((ref member, _)) = changes {
+        let mut nr = nodes.write();
+        if let Some(ref mut node) = nr.get_mut(&member.id) {
+            node.online = online;
+        }
+    }
 }
