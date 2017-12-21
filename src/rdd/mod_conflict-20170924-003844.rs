ac6888205ce7a5076fc866c6e916b97622506a27
Hao Shi <shisoftgenius@gmail.com>
2017-12-22 07:24:15 +0800

    plan changed

---
+++ src/rdd/mod_conflict-20170924-003844.rs
@@ -0,0 +1,34 @@
+mod funcs;
+mod transformations;
+use self::funcs::RDDFunc;
+use self::transformations::*;
+use super::contexts::task::TaskContext;
+use serde::{Deserialize, Serialize};
+
+pub trait Partition: Serialize {
+    fn index() -> u32;
+}
+pub trait Dependency: Serialize {
+    fn rdd<DD, T>() -> DD where DD: RDD<T>;
+}
+
+pub trait RDD<IN>: Serialize {
+
+    fn compute<P, ITER>(&self, partition: P, context: &TaskContext) -> ITER where ITER: Iterator, P: Partition;
+    fn get_partitions<P>(&self) -> Vec<P> where P: Partition;
+    fn get_dependencies<DEP>(&self) -> Vec<DEP> where DEP: Dependency;
+    fn id(&self) -> u64;
+
+    fn map<FN, OUT>(&self, func: FN) -> MapRDD<FN, IN, OUT> where FN: RDDFunc<IN, OUT> {
+        unimplemented!()
+    }
+    fn filter<FN>(&self, func: FN) -> FilterRDD<FN, IN> where FN: RDDFunc<IN, bool> {
+        unimplemented!()
+    }
+    fn flat_map<FN, OUT>(&self, func: FN) -> FlatMapRDD<FN, IN, OUT> where FN: RDDFunc<IN, OUT> {
+        unimplemented!()
+    }
+    fn map_partitions<FN, OUT>(&self, func: FN) -> MapPartitionsRDD<FN, IN, OUT> where FN: RDDFunc<IN, OUT> {
+        unimplemented!()
+    }
+}
--- src/storage/block/mod.rs
+++ src/storage/block/mod.rs
@@ -1,5 +1,5 @@
 // Block manager for shuffle and data sharing
-// Blocks will be saved at where it was calculated by block id hash. It's id will be registered on a raft state machine
+// Blocks will be saved at where it will be needed. It's id will be registered on a raft state machine
 // When shuffling, the block will be available on other nodes by fetching it from the node that generated it
 // Block is like a blob container without addressing table. It can be appended and read in sequel, lookup is impossible
 // Blocks also support lazy loading and streaming, which means it have a cursor so a request can fetch partial of it.
