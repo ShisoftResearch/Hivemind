545ac21607fd3e6333f712fed9802af52871a451
Hao Shi <shisoftgenius@gmail.com>
2018-02-17 22:45:19 +0800

    source from functions

--- src/resource/block_storage.rs
+++ src/resource/block_storage.rs
@@ -1,4 +1,4 @@
-use storage::block::{BlockManager, BlockCursor};
+use storage::block::{BlockManager, BlockCursor, ReadLimitBy};
 use futures::prelude::*;
 use utils::uuid::UUID;
 use parking_lot::Mutex;
@@ -15,6 +15,19 @@ pub struct BlockStorage<T> where T: DeserializeOwned {
     cursor: BlockCursor,
     fut: Option<BuffFut<T>>,
     buffer: Box<Iterator<Item = T>>
+}
+
+impl <T> BlockStorage<T> where T: DeserializeOwned + 'static {
+    pub fn new(manager: &Arc<BlockManager>, server_id: u64, id: UUID, buff_size: u64) -> BlockStorage<T> {
+        return BlockStorage {
+            server_id,
+            block_id: id,
+            manager: manager.clone(),
+            cursor: BlockCursor::new(id, ReadLimitBy::Items(buff_size)),
+            fut: None,
+            buffer: box vec![].into_iter()
+        }
+    }
 }
 
 impl <T> Stream for BlockStorage<T> where T: DeserializeOwned + 'static {
--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -1,16 +1,37 @@
 pub mod block_storage;
 
 use std::marker::PhantomData;
+use std::sync::Arc;
 use resource::block_storage::BlockStorage;
+use storage::block::BlockManager;
 use serde::de::DeserializeOwned;
+use futures::prelude::*;
+use futures::stream;
+use utils::uuid::UUID;
 
 pub enum Source<T> where T: DeserializeOwned {
-    Local(Box<IntoIterator<Item = T, IntoIter = Iterator<Item = T>>>),
+    Object(Box<Stream<Item = T, Error = ()>>),
     Block(BlockStorage<T>)
 }
 
+impl <II, T, I> From<II> for Source<T>
+    where T: DeserializeOwned,
+          I: Iterator<Item = T> + 'static,
+          II: IntoIterator<Item = T, IntoIter = I>
+{
+    fn from(source: II) -> Self {
+        let iter = source.into_iter();
+        return Source::Object(box stream::iter_ok(iter))
+    }
+}
 
+impl <T> Source<T> where T: DeserializeOwned + 'static {
+    pub fn from_block_storage(manager: &Arc<BlockManager>, server_id: u64, id: UUID, buff_size: u64) -> Self {
+        Source::Block(BlockStorage::new(manager, server_id, id, buff_size))
+    }
+}
 
 pub struct DataSet<T> where T: DeserializeOwned {
     source: Source<T>,
 }
+
