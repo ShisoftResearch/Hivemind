14029eed349a12b318a76881443a9b9531da7b79
Hao Shi <shisoftgenius@gmail.com>
2018-03-01 14:58:56 +0800

    rename for future iteration

--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -1,4 +1,5 @@
 pub mod block_storage;
+pub mod global_storage;
 
 use std::marker::PhantomData;
 use std::sync::Arc;
@@ -16,24 +17,24 @@ use actors::funcs::LocationTraced;
 
 pub const STORAGE_BUFFER: u64 = 10;
 
-pub enum SourceType {
+pub enum DataSetSourceType {
     Runtime,
     BlockStorage(BlockStorageProperty)
 }
 
 #[derive(Serialize, Deserialize)]
-pub enum SerdeData {
+pub enum SerdeDataSet {
     Runtime(Vec<Vec<u8>>),
     BlockStorage(BlockStorageProperty)
 }
 
 pub struct DataSet<T> where T: Serialize + DeserializeOwned {
     source: RefCell<Box<Stream<Item = T, Error = String>>>,
-    pub source_type: SourceType
+    pub source_type: DataSetSourceType
 }
 
 impl <T> DataSet<T> where T: Serialize + DeserializeOwned {
-    fn new(source: Box<Stream<Item = T, Error = String>>, source_type: SourceType) -> DataSet<T> {
+    fn new(source: Box<Stream<Item = T, Error = String>>, source_type: DataSetSourceType) -> DataSet<T> {
         DataSet { source: RefCell::new(source), source_type }
     }
 }
@@ -47,7 +48,7 @@ impl <II, T, I> From<II> for DataSet<T>
         let iter = source.into_iter();
         return DataSet::new(
             box stream::iter_ok(iter),
-            SourceType::Runtime)
+            DataSetSourceType::Runtime)
     }
 }
 
@@ -61,33 +62,33 @@ impl <T> DataSet<T> where T: Serialize + DeserializeOwned + 'static {
     ) -> Self {
         DataSet::new(
             box BlockStorage::new(manager, server_id, id, buff_size),
-            SourceType::BlockStorage(BlockStorageProperty {
+            DataSetSourceType::BlockStorage(BlockStorageProperty {
                 id, members, server_id
             }))
     }
-    fn ser_data(&self) -> SerdeData {
+    fn ser_data(&self) -> SerdeDataSet {
         match self.source_type {
-            SourceType::Runtime => {
+            DataSetSourceType::Runtime => {
                 let mut vec: Vec<Vec<u8>> = Vec::new();
                 let mut source = self.source.borrow_mut();
                 while let Ok(Async::Ready(item)) = source.poll() {
                     let data = bincode::serialize(&item);
                     vec.push(data)
                 }
-                SerdeData::Runtime(vec)
+                SerdeDataSet::Runtime(vec)
             },
-            SourceType::BlockStorage(ref prop) => SerdeData::BlockStorage(prop.clone())
+            DataSetSourceType::BlockStorage(ref prop) => SerdeDataSet::BlockStorage(prop.clone())
         }
     }
-    fn from_de_data(data: SerdeData) -> DataSet<T> {
+    fn from_de_data(data: SerdeDataSet) -> DataSet<T> {
         match data {
-            SerdeData::Runtime(ref data) => {
+            SerdeDataSet::Runtime(ref data) => {
                 Self::from(data
                     .iter()
                     .map(|item| bincode::deserialize(item))
                     .collect::<Vec<T>>())
             },
-            SerdeData::BlockStorage(prop) => {
+            SerdeDataSet::BlockStorage(prop) => {
                 Self::from_block_storage(
                     &BlockManager::default(),
                     prop.server_id, prop.id, prop.members, STORAGE_BUFFER)
@@ -111,7 +112,7 @@ impl <T> Serialize for DataSet<T> where T: Serialize + DeserializeOwned + 'stati
         S: Serializer
     {
         let ser_struct = self.ser_data();
-        SerdeData::serialize(&ser_struct, serializer)
+        SerdeDataSet::serialize(&ser_struct, serializer)
     }
 }
 
@@ -121,7 +122,7 @@ impl <'de, T> Deserialize<'de> for DataSet<T>
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
         where D: Deserializer<'de>
     {
-        let de_data = SerdeData::deserialize(deserializer)?;
+        let de_data = SerdeDataSet::deserialize(deserializer)?;
         Ok(Self::from_de_data(de_data))
 
     }
@@ -132,7 +133,7 @@ impl <T> LocationTraced for DataSet<T>
 {
     fn get_affinity(&self) -> Vec<u64> {
         match self.source_type {
-            SourceType::BlockStorage(ref prop) => prop.members.clone(),
+            DataSetSourceType::BlockStorage(ref prop) => prop.members.clone(),
             _ => vec![]
         }
     }
