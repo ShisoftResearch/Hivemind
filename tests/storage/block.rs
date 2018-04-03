7737845f16f8ab29d909f3a91fb4b756929ade67
Hao Shi <shisoftgenius@gmail.com>
2018-04-04 02:07:15 +0800

    require to fix block storage read

--- tests/storage/block.rs
+++ tests/storage/block.rs
@@ -1,7 +1,7 @@
 use hivemind::resource;
 use hivemind::server::*;
 use hivemind::utils::uuid::UUID;
-use hivemind::storage::block::Service;
+use hivemind::storage::block::*;
 use server::get_server;
 use futures::prelude::*;
 
@@ -12,6 +12,15 @@ pub fn primary() {
         storage: "test_data".to_owned()
     }, 5201);
     let task = UUID::rand();
+    let k1 = UUID::rand();
+    let k2 = UUID::rand();
+    let k3 = UUID::rand();
+    let k4 = UUID::rand();
+    let server_id = server.server_id;
     let block_manager = server.storage_managers.block.to_owned();
-    // block_manager.new_task(task);
+    block_manager.new_task(server_id, task);
+    block_manager.write(server_id, task, k1, vec![vec![1u8, 2u8, 3u8]]);
+    block_manager.write(server_id, task, k1, vec![vec![4u8, 5u8, 6u8]]);
+    let cursor = BlockCursor::new(task, k1, ReadLimitBy::Items(10));
+    let (data, new_cursor) = block_manager.read(server_id, cursor).wait().unwrap();
 }
